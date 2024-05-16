68d498dc31005412b1018e51730264af05f890bd
Ericphoto35 <150171033+Ericphoto35@users.noreply.github.com>
2024-05-16 13:19:02 +0200

    changement horaire integer

--- src/Controller/Admin/HorairesZooCrudController.php
+++ src/Controller/Admin/HorairesZooCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\HorairesZoo;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
 
 class HorairesZooCrudController extends AbstractCrudController
@@ -16,8 +17,8 @@ class HorairesZooCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            TimeField::new('debut'),
-            TimeField::new('fin'),
+            IntegerField::new('debut'),
+            IntegerField::new('fin'),
         ];
     }
 }
