460e802599163f464126550f258b381e1974cd0e
Ericphoto35 <150171033+Ericphoto35@users.noreply.github.com>
2024-05-17 13:24:31 +0200

    coco changement

--- src/Controller/VueAnimal4Controller.php
+++ src/Controller/VueAnimal4Controller.php
@@ -12,28 +12,28 @@ use MongoDB\Client as MongoClient;
 class VueAnimal4Controller extends AbstractController
 {
     #[Route('/Coco', name: 'app_coco')]
-    private function getViewCount(string $pageId): int
-{
-    $mongoClient = new MongoClient('mongodb://Ericdu974:Biloute974@lon5-c12-2.mongo.objectrocket.com:43741,lon5-c12-1.mongo.objectrocket.com:43741,lon5-c12-0.mongo.objectrocket.com:43741');
-    $db = $mongoClient->view_cococounter;
-    $collection = $db->page_views;
-
-    $filter = ['app_coco' => $pageId];
-    $result = $collection->findOne($filter);
-
-    return $result ? $result['view_count'] : 0;
-}
-
-
-    #[Route('/vues', name: 'app_vues')]
-    
-    public function index(): Response
+    public function index(EntityManagerInterface $EntityManager): Response
     {
-        $pageId = 'app_coco'; // Replace with your actual page ID
-        $viewCount = $this->getViewCount($pageId);
 
+        $animal = $EntityManager->getRepository(Animals::class)->findOneBy(['prenomani' => 'Coco']);
+        $mongoClient = new MongoClient('mongodb://Ericdu974:Biloute974@lon5-c12-2.mongo.objectrocket.com:43741,lon5-c12-1.mongo.objectrocket.com:43741,lon5-c12-0.mongo.objectrocket.com:43741/Arcadia?replicaSet=dea02bd29b77453680af2162ec6f8654');
+        $db = $mongoClient->view_cococounter;
+        $collection = $db->page_views;
+
+        $pageId = 'app_coco'; // Replace with your actual page ID
+
+        $filter = ['app_coco' => $pageId];
+        $update = ['$inc' => ['view_count' => 1]];
+        $options = ['upsert' => true];
+
+        $updateResult = $collection->updateOne($filter, $update, $options);
+         if ($updateResult->getModifiedCount() === 1) {
+            echo "View count for page $pageId incremented successfully.\n";
+        } else {
+            echo "View count for page $pageId could not be incremented.\n";
+        }
         return $this->render('vue_animal/animal4.html.twig', [
-            'viewCount' => $viewCount,
+            'animals' => $animal,
         ]);
     }
 }
